552c57f97df7919ecb7a0dc2236475d9ba688f61
patrickleon <leon.patrick19@gmail.com>
2017-07-19 16:12:37 -0500

    finally done

--- overly-excited.js
+++ overly-excited.js
@@ -1,14 +1,19 @@
 console.log("Hello, world!")
 // Create an array that contains the words in the sentence
-var sentence = ["The", "walrus", "danced!", "through", "the", "trees", "in", "the", "light", "of", "the", "moon"]
+var sentence = ["The", "walrus", "danced", "through", "the", "trees", "in", "the", "light", "of", "the", "moon"]
 console.log()
 // The addExcitement function should accept the array as the sole argument
 function addExcitement (theWordArray) {
 
 var line = ""; 
 
-for (var i = 0; i < sentence.length; i++) {
-line = line + sentence[i] + " ";
+for (var i = 0; i < sentence.length; i++) { 
+	line = line + sentence[i] + " ";
+	if ((i + 1) % 3 === 0) 
+	{
+		line += "!"
+	}
+
  console.log(line);	
 
 
